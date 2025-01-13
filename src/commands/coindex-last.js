66732009924ebd858613dc449d6a98d6513dea0a
João Victor Vilela dos Santos <148297412+vilelacc@users.noreply.github.com>
2025-01-13 08:36:00 -0300

    CHORE: coomand last

--- src/commands/coindex-last.js
+++ src/commands/coindex-last.js
@@ -1,14 +1,14 @@
 import { Command } from "commander";
 import { Coindex } from "../libs/coindex.js";
 
-const lastCommand = new Command("last")
+const LastCommand = new Command("last")
   .description("returns the last occurrence of the selected currencies.")
   .argument(
     "<currency_code>",
     "Selected currencies separated by comma (,) Ex.: USD-BRL,EUR-BRL,BTC-BRL"
   )
-  .action((currencyCode) => {
-    Coindex.showLast(currencyCode.split(" ").join(","));
+  .action((CurrencyCode) => {
+    Coindex.showLast(CurrencyCode.split(" ").join(","));
   });
 
-export default lastCommand;
+export default LastCommand;
